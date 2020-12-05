014080cbfc26aa0bac0b26f0df89a00638ef44c2
Fedor Boretskiy <fedor.boretsky@gmail.com>
2020-12-06 01:27:37 +0300

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Fedor Boretskiy on 06.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
